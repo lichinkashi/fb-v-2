bd380fb60a364e1be31febb9606e4a7514c2a9bc
lichinkashi <lichinkashi@gmail.com>
2025-11-23 14:05:56 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'flashback-v1';
+const urlsToCache = [
+  '/fb-webg/',
+  '/fb-webg/index.html',
+  '/fb-webg/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
